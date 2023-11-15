6306b1e8229fbe44fa13d2eba184d2ee41a84c9f
KIKI0911 <n.daiki911@gmail.com>
2023-11-15 16:39:49 +0900

    lambok追加

--- src/main/java/com/country/name/NameSearchRequest.java
+++ src/main/java/com/country/name/NameSearchRequest.java
@@ -1,37 +1,12 @@
 package com.country.name;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class NameSearchRequest {
     private String startsWith;
     private String endsWith;
     private String contains;
-
-    public NameSearchRequest(String startsWith, String endsWith, String contains) {
-        this.startsWith = startsWith;
-        this.endsWith = endsWith;
-        this.contains = contains;
-    }
-
-    public String getStartsWith() {
-        return startsWith == null ? "": startsWith;
-    }
-
-    public void setStartsWith(String startsWith) {
-        this.startsWith = startsWith;
-    }
-
-    public String getEndsWith() {
-        return endsWith == null ? "": endsWith;
-    }
-
-    public void setEndsWith(String endsWith) {
-        this.endsWith = endsWith;
-    }
-
-    public String getContains() {
-        return contains == null ? "": contains;
-    }
-
-    public void setContains(String contains) {
-        this.contains = contains;
-    }
 }
